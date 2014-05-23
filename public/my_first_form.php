07761cdcc0ec28e887b22663798d9db80b4de480
Justin <Justin@Justins-MacBook-Pro.local>
2014-05-23 10:44:28 -0500

    Built a to & from email field, a subject box, and message body field, and a submit button

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -9,6 +9,7 @@
     var_dump($_GET);
     var_dump($_POST);
     ?>
+        <h3>User Login</h3>
         <form method="POST" action="/my_first_form.php">
             <p>
                 <label for="username">Username</label>
@@ -19,8 +20,35 @@
                 <input id="password" name="password" type="password" placeholder="password here">
             </p>
             <p>
-                <input type="button" value="Login">
+                <input type="submit" value="Login">
             </p>
         </form>
+
+        <h3>Compose an Email</h3>
+        <form method="Post" action="/my_first_form.php">
+            <p>
+                <label for="To">To</label>
+                <input id="To" name="To" type="email" placeholder="email address here">
+            </p>
+            <p>
+                <label for="From">From</label>
+                <input id="From" name="From" type="email" placeholder="email address here">
+            </p>
+            <p>
+                <label for="Subject">Subject</label>
+                <input id="Subject" name="Subject" type="text" placeholder="subject">
+            </p>
+             <p>
+                <label for="email_body">Email Body</label>
+                <textarea id="email_body" name="email_body" rows="20" cols="100" type="text">
+            </textarea>
+
+            <p>
+                <input type="submit" value="Send">
+
+            </p>
+        </form> 
+
+
     </body>
 </html>
