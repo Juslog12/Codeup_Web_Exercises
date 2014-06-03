38f7e594a0bcb3073fce08481bf9def9193a9cca
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-03 10:25:18 -0500

    Updated Address book to a table from a list

--- public/address_book.php
+++ public/address_book.php
@@ -3,35 +3,33 @@ $contacts= $_POST;
 $headings= ['name', 'address', 'city', 'state', 'zip', 'phone'];
 
 $handle = fopen('contacts.csv', 'w');
-$errormessage = "";
-
 // ($contacts as $contact) {
 	fputcsv($handle, $contacts);
 // }
 
 fclose($handle);
 
+//used array_push to put the two arrays together to build the list of contacts
 
 ?>
 <!DOCTYPE html>
 	<html>
 	<head>
 		<meta charset="utf-8">
-		<title>TODO List</title>
+		<title>Address Book</title>
 	</head>
 	<body>	
-	<?
-	    var_dump($_POST);
-    ?>
+	
 
 
 <h3>Address Book</h3>
-	<dl>
+	<table border= 1>
+		<tr><th>Name</th><th>Address</th><th>City</th><th>State</th><th>Zip</th><th>Phone</th></tr>
 	<? foreach ($contacts as $key => $value): ?>
 		<? if(empty($value)): ?>
-		<?php echo "$key " . "  incomplete"; ?><br>
+		<?php echo "<td> " . "  Empty field!</td> "; ?>
 		<?php else : ?>
-		<?php echo "$key $value" ?>
+		<?php echo "<td> $value</td> " ?>
 <!-- 			<li><?= htmlspecialchars(strip_tags($username)) ?></li>
 		
 			<li><?= htmlspecialchars(strip_tags($address)) ?></li>	
@@ -45,22 +43,27 @@ fclose($handle);
 			<li><?= htmlspecialchars(strip_tags($phone)) ?></li> -->				
 		<? endif; ?>
 	<? endforeach; ?>	
-	</dl>		
+	</table>		
 
-	<h3>Contact Information</h3>
+	<h3>Contact Info</h3>
 
 	<form method="POST" action="/address_book.php">
 			<p>
-				<label for="Username">Username</label>
-				<input id="Username" name="Username" type="text" autofocus = "autofocus"><br></input>
+				<label for="Name">Name</label>
+				<input id="Name" name="Name" type="text" autofocus = "autofocus"><br></input>
+				
 				<label for="Address">Address</label>
 				<input id="Address" name="Address" type="" autofocus = "autofocus"><br></input>
+				
 				<label for="City">City</label>
 				<input id="City" name="City" type="text" autofocus = "autofocus"><br></input>
+				
 				<label for="State">State</label>
 				<input id="State" name="State" type="text" autofocus = "autofocus"><br></input>
+				
 				<label for="Zip">Zip</label>
 				<input id="Zip" name="Zip" type="text" autofocus = "autofocus"><br></input>
+				
 				<label for="Phone">Phone</label>
 				<input id="Phone" name="Phone" type="text" autofocus = "autofocus"><br></input>	
 			</p>
