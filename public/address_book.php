8a04c2aabf0c3bdc28f5b9e09c1e2ad6375c9b03
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-04 09:49:31 -0500

    Built the address to include classes

--- public/address_book.php
+++ public/address_book.php
@@ -1,77 +1,118 @@
 <?
-$contacts= $_POST;
-$headings= ['name', 'address', 'city', 'state', 'zip', 'phone'];
+$address_book = [];
+$errorMessage = '';
+$filename = 'contacts.csv';
 
-$handle = fopen('contacts.csv', 'w');
-// ($contacts as $contact) {
-	fputcsv($handle, $contacts);
-// }
+class AddressDataStore 
+{
+	public $filename = '';
+	function readAddressBook()
+	{
+		$addresses = [];
+		$handle = fopen($this->filename, 'r');
+		$addresses = [];
+			while(!feof($handle)) 
+			{
+				$row = fgetcsv($handle);
+				if(!empty($row))
+				{		
+				$addresses[] = $row;
+				}
+			}
+		fclose($handle);		
+		return $addresses;
+	
+	}
+	function writeAddressBook($addresses_array)
+	{
+		$handle = fopen($this->filename, 'w');
+			foreach ($addresses_array as $fields) 
+			{
+				fputcsv($handle, $fields);
+			}
+		fclose($handle);
+	}	
+}
 
-fclose($handle);
+$ads = new AddressDataStore();
+$ads->filename = 'contacts.csv';
 
-//used array_push to put the two arrays together to build the list of contacts
+$addressBook = $ads->readAddressBook();
+
+if (!empty($_POST))
+{ 
+	if(!empty($_POST['Name']) && !empty($_POST['Address']) && !empty($_POST['City']) && !empty($_POST['State']) && !empty($_POST['Zip'])&& !empty($_POST['Phone']))
+	{
+		$newAddress = [];
+		$newAddress['Name'] = $_POST['Name'];
+		$newAddress['Address'] = $_POST['Address'];
+		$newAddress['City'] = $_POST['City'];
+		$newAddress['State'] = $_POST['State'];
+		$newAddress['Zip'] = $_POST['Zip'];
+		$newAddress['Phone'] = $_POST['Phone'];
+
+		$addressBook[] = $newAddress;
+	$ads->writeAddressBook($addressBook);
+	}
+	else
+	{
+		$errorMessage = "Validation failed. Please complete all fields.";
+	}
+}
 
 ?>
 <!DOCTYPE html>
 	<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Address Book</title>
-	</head>
-	<body>	
-	
-
-
-<h3>Address Book</h3>
-	<table border= 1>
-		<tr><th>Name</th><th>Address</th><th>City</th><th>State</th><th>Zip</th><th>Phone</th></tr>
-	<? foreach ($contacts as $key => $value): ?>
-		<? if(empty($value)): ?>
-		<?php echo "<td> " . "  Empty field!</td> "; ?>
-		<?php else : ?>
-		<?php echo "<td> $value</td> " ?>
-<!-- 			<li><?= htmlspecialchars(strip_tags($username)) ?></li>
-		
-			<li><?= htmlspecialchars(strip_tags($address)) ?></li>	
-		
-			<li><?= htmlspecialchars(strip_tags($city)) ?></li>
-	
-			<li><?= htmlspecialchars(strip_tags($state)) ?></li>
-		
-			<li><?= htmlspecialchars(strip_tags($zip)) ?></li>
-		
-			<li><?= htmlspecialchars(strip_tags($phone)) ?></li> -->				
-		<? endif; ?>
-	<? endforeach; ?>	
-	</table>		
-
-	<h3>Contact Info</h3>
-
-	<form method="POST" action="/address_book.php">
-			<p>
-				<label for="Name">Name</label>
-				<input id="Name" name="Name" type="text" autofocus = "autofocus"><br></input>
-				
-				<label for="Address">Address</label>
-				<input id="Address" name="Address" type="" autofocus = "autofocus"><br></input>
-				
-				<label for="City">City</label>
-				<input id="City" name="City" type="text" autofocus = "autofocus"><br></input>
-				
-				<label for="State">State</label>
-				<input id="State" name="State" type="text" autofocus = "autofocus"><br></input>
-				
-				<label for="Zip">Zip</label>
-				<input id="Zip" name="Zip" type="text" autofocus = "autofocus"><br></input>
-				
-				<label for="Phone">Phone</label>
-				<input id="Phone" name="Phone" type="text" autofocus = "autofocus"><br></input>	
-			</p>
+		<head>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<title>Address Book</title>
+			<meta name= "description" content="">
+		</head>
+		<body>	
+		<h3>Address Book</h3>
 			<p>	
-				<input type="submit">		
-			</p>
-
-
+			<table border="1">
+				<tr>
+					<th>Name</th>
+					<th>Address</th>
+					<th>City</th>
+					<th>State</th>
+					<th>Zip</th>
+					<th>Phone</th>
+				</tr>
+			<? foreach ($addressBook as $index => $row): ?>
+			<tr>
+				<? foreach ($row as $column): ?>		
+					<td><?= $column;?></td>
+				<? endforeach; ?>
+			</tr>
+		<? endforeach; ?>	
+		</table>
+		</p>
+	<form method="POST" action="address_book.php">
+		<p>
+			<label for="Name">Name</label>
+			<input id="Name" name="Name" type="text" autofocus = "autofocus"><br></input>
+			
+			<label for="Address">Address</label>
+			<input id="Address" name="Address" type="" autofocus = "autofocus"><br></input>
+			
+			<label for="City">City</label>
+			<input id="City" name="City" type="text" autofocus = "autofocus"><br></input>
+			
+			<label for="State">State</label>
+			<input id="State" name="State" type="text" autofocus = "autofocus"><br></input>
+			
+			<label for="Zip">Zip</label>
+			<input id="Zip" name="Zip" type="text" autofocus = "autofocus"><br></input>
+			
+			<label for="Phone">Phone</label>
+			<input id="Phone" name="Phone" type="text" autofocus = "autofocus"><br></input>	
+		</p>
+		<p>	
+			<input type="submit">		
+		</p>
 	</form>
 </body>
 </html>
