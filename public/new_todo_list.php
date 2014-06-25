7fed4e47ed6b4f18be7cded41d6cff4acba79e4c
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-25 13:31:49 -0500

    Updated remove button to fix a bug in the POST.

--- public/new_todo_list.php
+++ public/new_todo_list.php
@@ -28,35 +28,39 @@ $dbc->exec($query);
 */
 
 
-/*if(count($_FILES) > 0 && $_FILES['file1']['error'] == 0) 
-{
-    if($_FILES['file1']['type'] == 'text/plain') 
-    {
-        $upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
-        $filename = basename($_FILES['file1']['name']);
-        $saved_filename = $upload_dir . $filename; 
-        move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
-        $new_items = readtheFile($saved_filename);
-        $list_items = array_merge($list_items, $new_items);
-    } 
-    else 
-    {
-        $error_message = "Error! Not a valid form type!";
-    }
-} 
-*/
-
 define ('DEFAULT_LIMIT', 10);
+
+if(!empty($_POST))
+{
+    //$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'justin', 'Maslog12');
+
+    //$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+    if (!empty($_POST['todo_item']))
+    {
+        $stmt = $dbc->prepare('INSERT INTO new_todo_list (todo_item) VALUES (:todo_item)');
+        $stmt->bindValue(':todo_item', $_POST['todo_item'], PDO::PARAM_STR);
+
+        $stmt->execute();
+    }
+    elseif (!empty($_POST['remove'])) 
+    {
+        $stmt = $dbc->prepare('DELETE FROM new_todo_list WHERE id = :id');
+        $stmt->bindValue(':id', $_POST['remove'], PDO::PARAM_INT);
+
+        $stmt->execute();
+    }
+}
+
 
 function getItems($dbc) 
 {
    //return $dbc->query('SELECT * FROM national_parks LIMIT 4 OFFSET ' . getOffset())->fetchAll(PDO::FETCH_ASSOC);
     $stmt = $dbc->prepare('SELECT * FROM new_todo_list LIMIT :limit OFFSET :offset');
-        $stmt->bindValue(':limit', DEFAULT_LIMIT, PDO:: PARAM_INT);
-        $stmt->bindValue(':offset', getOffset(), PDO:: PARAM_INT);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+    $stmt->bindValue(':limit', DEFAULT_LIMIT, PDO:: PARAM_INT);
+    $stmt->bindValue(':offset', getOffset(), PDO:: PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
 function getOffset()
 { 
@@ -71,29 +75,9 @@ $page = isset($_GET['page']) ? $_GET['page'] : 1;
 $nextPage = $page + 1;
 $prevPage = $page - 1;
 
-if(!empty($_POST))
-{
-    //$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'justin', 'Maslog12');
 
-    //$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
 
-    $stmt = $dbc->prepare('INSERT INTO new_todo_list (todo_item) VALUES (:todo_item)');
-        $stmt->bindValue(':todo_item', $_POST['todo_item'], PDO::PARAM_STR);
 
-        $stmt->execute();
-}
-
-if (isset($_POST['remove-id'])) 
-{
-    $stmt = $dbc->prepare('DELETE FROM todo
-        WHERE id = :id');
-        $stmt->bindValue(':id', $_POST['remove'], PDO::PARAM_INT);
-
-        $stmt->execute();
-}
-
-$list = getItems($dbc);
-//var_dump($list);
 
 ?>
 
@@ -110,7 +94,7 @@ $list = getItems($dbc);
     <table class="table table-hover" align= "center" border = black 1px solid>
         <th>Id</th>
         <th>Todo Item</th>
-        <? foreach($list as $rows): ?>
+        <? foreach(getItems($dbc) as $rows): ?>
         <tr>
             <td><?= $rows['id'] ?></td>   
             <td><?= $rows['todo_item'] ?><button class="btn btn-danger btn-sm pull-right btn-remove" data-todo="<?= $rows['id']; ?>">Remove</button></td>
@@ -125,20 +109,33 @@ $list = getItems($dbc);
 <? endif ?>
 
 <h3>Add New Item</h3>
-    <form method="POST" type="text/css" enctype="multipart/form-data" action="new_todo_list.php">
-       
-        <p>
-            <label for="todo_item">Todo Item</label>
-            <input id="todo_item" name="todo_item" type="text" autofocus = "autofocus"></input>
-        </p>
-        <p> 
-            <input type="submit" value="Add">       
-        </p>
-    </form> 
-     <form id="removeForm" action="todo-db.php" method="post">
-        <input id="remove-id" type="hidden" name="remove" value="">
-    </form>
+<form method="POST" type="text/css" enctype="multipart/form-data" action="new_todo_list.php">
+   
+    <p>
+        <label for="todo_item">Todo Item</label>
+        <input id="todo_item" name="todo_item" type="text" autofocus = "autofocus"></input>
+    </p>
+    <p> 
+        <input type="submit" value="Add">       
+    </p>
+</form> 
+ 
 
+<h3>Upload File</h3>
+
+<form method="POST" enctype="multipart/form-data">
+    <p>
+        <label for="file1">File to upload: </label>
+        <input type="file" id="file1" name="file1">
+    </p>
+    <p>
+        <input type="submit" value="Upload">
+    </p>
+</form>
+
+<form id="remove-form" action="new_todo_list.php" method="post">
+    <input id="remove-id" type="hidden" name="remove" value="">
+</form>
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script>
 
